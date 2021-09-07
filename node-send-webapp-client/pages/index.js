0571ffe9f8af753450e57fadd91447dc6cca6f04
David L <dvdlq99@gmail.com>
2021-09-07 15:59:42 +0200

    Created node-send-webapp-client, Layout and Header components

---
+++ node-send-webapp-client/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Index = () => {
+  return (
+    <div className="container">
+      <Layout>
+        <h1>home</h1>
+      </Layout>
+      
+    </div>
+  )
+}
+
+export default Index;
---
+++ node-send-webapp-client/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Login = () => {
+  return (
+    <div className="container">
+      <Layout>
+        <h1>Login</h1>
+      </Layout>
+      
+    </div>
+  )
+}
+
+export default Login;
