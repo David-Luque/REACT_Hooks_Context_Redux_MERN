ea4dec8eff404cf7df18952c655db7fd8cb560ff
David L <dvdlq99@gmail.com>
2021-09-28 16:48:00 +0200

    Created ArtistInfo component and finished Song component

--- lyrics-app/src/App.js
+++ lyrics-app/src/App.js
@@ -1,26 +1,49 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Form from './components/Form.js';
+import Song from './components/Song';
+import ArtistInfo from './components/ArtistInfo';
 import axios from 'axios';
 
 function App() {
 
   const [ lyricSearch, setLyricSearch ] = useState({});
   const [ lyrics, setLyrics ] = useState('');
+  const [ artistInfo, setArtistInfo ] = useState({});
 
   useEffect(()=>{
     if(Object.keys(lyricSearch).length === 0) return;
-  
-    const lyricRequest = async ()=>{
+    const lyricsInfoRequest = async ()=>{
       const { artist, song } = lyricSearch;
-      const result = await axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`);
-      setLyrics(result.data.lyrics)
-    }
-    lyricRequest();
+      //use Promise.all() to make both GET request at same time, and continue when both are done
+      const [ lyric, artistInfoApi ] = await Promise.all([
+        axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`),
+        axios.get(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`)
+      ]);
+      setLyrics(lyric.data.lyrics);
+      setArtistInfo(artistInfoApi.data.artists[0])
+    };
+    lyricsInfoRequest();
   }, [lyricSearch]);
+
+
 
   return (
     <Fragment>
       <Form  setLyricSearch={setLyricSearch} />
+      <div className="container mt-5">
+        <div className="row">
+          <div col-md-6>
+            <ArtistInfo
+              artistInfo={artistInfo}
+            />
+          </div>
+          <div col-md-6>
+            <Song 
+              lyrics={lyrics}
+            />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
