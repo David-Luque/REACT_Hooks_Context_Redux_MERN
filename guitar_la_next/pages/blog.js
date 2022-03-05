f3e7c6ce92449beafed17d8890780cf410966583
David L <dvdlq99@gmail.com>
2022-03-05 17:23:03 +0100

    Created function getServersidePorps to request for blog entries

--- guitar_la_next/pages/blog.js
+++ guitar_la_next/pages/blog.js
@@ -1,6 +1,20 @@
 import Layout from '../components/Layout';
 
-export default function Blog() {
+export async function getServerSideProps() { //in this case, function "getStaticsProps , witch will be the function that we will use, is exactly the same. we need only change the name"
+    const url = 'http://localhost:1337/blogs';
+    const response = await fetch(url);
+    const data = await response.json();
+    return {
+        props: {
+            data // this data will be available automatically in this page because of export expresion
+        }
+    }
+}; //whit "export", this function will be abailable in the rest of this same page
+
+export default function Blog({ data }) {
+
+    console.log(data)
+
     return (
         <Layout page={"Blog"}>
             <div>
@@ -9,3 +23,4 @@ export default function Blog() {
         </Layout>
     );
 }; 
+
